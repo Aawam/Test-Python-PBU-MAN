d8206595eb1eeaaa86a10c492e694928065f93c3
RexsyBima <Rexsy.Bimq12@gmail.com>
2024-02-20 16:47:17 +0700

    Initialize branch, also fix scraping

--- Code.py
+++ Code.py
@@ -1,46 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+
 #
-html = requests.get('https://pbu.mayoga.sch.id/data-pendaftar-pbu-2024/')
-soup = BeautifulSoup(html.text, 'html.parser')
+html = requests.get("https://pbu.mayoga.sch.id/data-pendaftar-pbu-2024/")
+soup = BeautifulSoup(html.text, "html.parser")
 #
-table = soup.find_all('th')
+table = soup.find_all("th")
 header = [title.text.strip() for title in table]
-df = pd.DataFrame(columns = header)
+df = pd.DataFrame(columns=header)
 
 #
-max_student = soup.find_all('tr')[-1].find_all('td')[0].text
-#When do iteration use this formula
-#while i <= max_student: Loop \ i++
+max_student = soup.find_all("tr")[-1].find_all("td")[0].text
+# When do iteration use this formula
+# while i <= max_student: Loop \ i++
 
-num = soup.find('tbody')
-raw = num.find_all('tr')
+table = soup.find("tbody")
+datas = table.find_all("tr")
+output = []
+for i in datas:
+    i = [x.text for x in i.find_all("td")]
+    i: BeautifulSoup = i
 
-for raw_data in raw: 
-    cek = raw_data.find('td')
-    
-    for data in cek:
-        data1 = data.find(class_='numdata integer column-no sorting_1').text()
-        print(data1)
-    
-    """
     result = {
-        "No": Student_data[0],
-        "ID": Student_data[1],
-        "Nama": Student_data[2],
-        "Date": Student_data[3],
-        "Choose to Boarding": Student_data[4],
-        "Verif Result": Student_data[5],
-        "Test Date": Student_data[6]
+        "ID": i[0],
+        "Reg_Code": i[1],
+        "Name": i[2],
+        "Date": i[3],
+        "isBoarded": True if i[4] == "YA" else False,
+        "Verif": True if i[5] == "LOLOS" else False,
+        "Interview_Date": i[6],
     }
-    """
+    output.append(result)
 
-#length = len(df)
-#df.loc[length] = result
+for i in output:
+    print(i)
+
+df = pd.DataFrame(data=output)
+df.to_csv("output.csv", index=False)
+# length = len(df)
+# df.loc[length] = result
 
 
-#Student_data = [data.text.strip() for data in cek]
+# Student_data = [data.text.strip() for data in cek]
 
 """
 for raw in num:
